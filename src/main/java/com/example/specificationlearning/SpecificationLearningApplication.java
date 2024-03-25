129d578842283709b97103f12dedecd46be056cb
Andrei Guzei <aaguzey@gmail.com>
2024-03-25 20:31:29 +1000

    Initial commit

---
+++ src/main/java/com/example/specificationlearning/SpecificationLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.specificationlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecificationLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecificationLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/specificationlearning/SpecificationLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.specificationlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecificationLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
